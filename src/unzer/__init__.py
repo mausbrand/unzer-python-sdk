4516397d52caf59ac836f5da90c5d10bd8bfc184
Sven Eberth <mail@sveneberth.de>
2024-02-22 01:52:01 +0100

    Bump version

--- src/unzer/__init__.py
+++ src/unzer/__init__.py
@@ -1,7 +1,7 @@
 __title__ = "unzer-sdk"
 __author__ = "Sven Eberth"
 __email__ = "se@mausbrand.de"
-__version__ = "1.0.0-dev3"
+__version__ = "1.0.0-dev4"
 
 from .client import UnzerClient
 from .model import *
