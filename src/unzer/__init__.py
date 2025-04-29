03b9bc2dacd8607ba3a05df44511d097a2eaf3a5
Sven Eberth <mail@sveneberth.de>
2025-04-29 17:41:33 +0200

    chore: bump version to v1.0.0-dev6

--- src/unzer/__init__.py
+++ src/unzer/__init__.py
@@ -1,7 +1,7 @@
 __title__ = "unzer-sdk"
 __author__ = "Sven Eberth"
 __email__ = "se@mausbrand.de"
-__version__ = "1.0.0-dev5"
+__version__ = "1.0.0-dev6"
 
 from .client import UnzerClient
 from .model import *
